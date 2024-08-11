09f28d16c47095837ecbe11a191c87afe3c5360b
Bianca Mendes <biancamendesara@gmail.com>
2024-08-11 19:28:43 -0300

    Versão inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+var Leonardo = window.document.getElementById("Leonardo")
+var Samantha = window.document.getElementById("Samantha")
+var Bruna = window.document.getElementById("Bruna")
+
+function RolarParaDireita(){
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+     setaDireita.style= "display:none"
+    setaEsquerda.style= "display.flex; margin-top:55%"
+    
+}
+function RolarParaEsquerda(){
+    Leonardo.style= "display:flex"
+    Bruna.style = "display:none"
+    setaDireita.style="display.flex; margin-top:55%"
+}
